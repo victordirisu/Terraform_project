221e3da9a9fcbb090a95034ba14afd024660c0dc
victordirisu <147115134+victordirisu@users.noreply.github.com>
2024-02-04 15:16:36 +0100

    Create main.tf

---
+++ terraform-scripts/main.tf
@@ -0,0 +1,57 @@
+provider "azurerm" {
+  features = {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "AKS_RG"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks_cluster" {
+  name                = "myAKSCluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+
+  dns_prefix = "myakscluster"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  enable_rbac = true
+}
+
+resource "azurerm_virtual_network" "aks_vnet" {
+  name                = "aksVnet"
+  address_space       = ["10.0.0.0/3"]
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+}
+
+resource "azurerm_subnet" "aks_subnet" {
+  name                 = "aksSubnet"
+  resource_group_name  = azurerm_resource_group.aks_rg.name
+  virtual_network_name = azurerm_virtual_network.aks_vnet.name
+  address_prefixes     = ["10.0.1.0/18"]
+}
+
+#incorporating variables for better parametrization
+
+variable "resource_group_name" {
+  type = string
+  default = "AKS-RG"
+}
+
+output "aks_cluster_id" {
+  value = azurerm_kubernetes_cluster.aks_cluster.id
+}
