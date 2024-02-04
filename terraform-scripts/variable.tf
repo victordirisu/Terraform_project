79a0b012398146f47aca90c9c95366f347bb2446
victordirisu <147115134+victordirisu@users.noreply.github.com>
2024-02-04 15:25:49 +0100

    Update variable.tf

--- terraform-scripts/variable.tf
+++ terraform-scripts/variable.tf
@@ -6,12 +6,15 @@ variable "azure_cluster" {
   description = "Name of the AKS cluster for project"
 }
 
-# define variables related to Role-Based Access Control(RBAC)
-
 variable "rbac_role" {
   description = "Name of the custom RBAC role definition"
 }
 
 variable "user_principal_id" {
   description = "Principal ID of the user or service principal to assign the role"
+}
+
+variable "resource_group_name" {
+  type = string
+  default = "AKS-RG"
 }
